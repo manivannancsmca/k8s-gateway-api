33bb2055e4aebc7a27beeb58964cccc0b7ba9af8
manivannancsmca <manivannancsmca@gmail.com>
2026-06-17 14:26:45 +0530

    first commit

---
+++ src/main/java/com/k8s_gateway_api/K8sGatewayApiApplication.java
@@ -0,0 +1,13 @@
+package com.k8s_gateway_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sGatewayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sGatewayApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/k8s_gateway_api/K8sGatewayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k8s_gateway_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sGatewayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
